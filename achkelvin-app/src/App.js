2efb9966de9243a830157d22951e50b87af5a6fe
achkelvin <a.kelvintri@gmail.com>
2022-03-23 09:55:36 +0700

    hw1m2

---
+++ achkelvin-app/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  const kelas = "KMA";
+  const baik = "baik1";
+      <img src={logo} className="App-logo" alt="logo" />
+      return (
+        <div className="App">
+
+          <div>
+            <img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/05/28/3661992353.jpg"></img>
+            <table>
+              <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Albums</th>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>deja vu</td>
+                <td>Olivia Rodrigo</td>
+                <td>SOUR</td>
+              </tr>
+			      </table>
+            <button class="buttonSong" type="button">Select</button>
+          </div>
+          
+        </div>
+      );
+}
+
+export default App;
