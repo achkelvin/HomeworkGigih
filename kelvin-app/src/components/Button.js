75a8b8ef4d3ec8e118cb5dcbfc1698e32202d3c1
achkelvin <a.kelvintri@gmail.com>
2022-03-25 00:10:51 +0700

    homework2 module2

---
+++ kelvin-app/src/components/Button.js
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+
+export class Button extends Component{
+
+    render(){
+        return(
+            <button class="buttonSong" type="button">Select</button>
+        )
+    }
+}
