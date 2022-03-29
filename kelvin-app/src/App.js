9a93d557e773b3f500a78b4d5aae8f5ede6a9985
achkelvin <a.kelvintri@gmail.com>
2022-03-30 01:21:57 +0700

    Update

--- kelvin-app/src/App.js
+++ kelvin-app/src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './App.css';
 import Data from "./data/Data";
-import Song from "./components/index"
+import Song from "./components/Tracks"
 
 
 function App() {
     
-      return (
-        <div className="container">
+  return (
+      <div className="container">
       <div className="Header">
-        <h1>Song Playlist</h1>
+        <h1>Playlist</h1>
       </div>
 
         
-        {Data
-        .map((d) => (
-          <trackSong
+        {Data.map((d) => (
+          <Song
          image={d.album.images[0].url} 
          album={d.name} 
          artist={d.artists[0]?.name} 
--- kelvin-app/src/components/Tracks.js
+++ kelvin-app/src/components/Tracks.js
@@ -8,7 +8,7 @@ function Song(props) {
         <h1>{props.title}</h1>
         <h2>Album : {props.album}</h2>
         <h2>Artist : {props.artist}</h2>
-        <button className="btn">Select</button>
+        <button className="btn">Play</button>
       </div>
     </div>
   );
