b77f83d2030936ba5f18193ff8b86ef19dde9561
achkelvin <a.kelvintri@gmail.com>
2022-04-22 13:40:19 +0700

    Homework 2 Module 5

--- kelvin-app/src/components/Item/Search.js
+++ kelvin-app/src/components/Item/Search.js
@@ -1,16 +1,16 @@
+import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
+
 const SearchForm = ({ onSubmit, onChange }) => {
-  return (
-      <div className="container">
-          <hr></hr>
-          <h2> Search Song</h2>
-          <form onSubmit={onSubmit}>
-              <input onChange={onChange} />
-              <br></br>
-              <button type="submit">
-                  Search
-              </button>
-          </form>
-      </div>
+    return (
+        <div >
+            <h2> Search Song</h2>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} />
+                <br></br> <br></br>
+                <Button type="submit" value="Submit" variant="contained" color="success" startIcon={<SearchIcon />}> SEARCH </Button> {/* ADD NEW UI BUTTON FROM MUI */}
+            </form>
+        </div>
   )
 }
 
